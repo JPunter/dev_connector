8987815e78a55a0072414a0ff6e41f56cfe6f34e
Jacob Punter <jacob.punter@collinsongroup.com>
2021-04-06 18:50:58 +0100

    Added all profiles routes and github repo lists

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -4,19 +4,73 @@ import { setAlert } from "./alert";
 
 import {
 	GET_PROFILE,
+	GET_PROFILES,
+	GET_REPOS,
 	UPDATE_PROFILE,
 	PROFILE_ERROR,
 	ACCOUNT_DELETED,
 	CLEAR_PROFILE,
 } from "./types";
 
-// Get current uses profile
+// Get current user profile
 export const getCurrentProfile = () => async (dispatch) => {
 	try {
 		const res = await api.get("/profile/me");
 
 		dispatch({
 			type: GET_PROFILE,
+			payload: res.data,
+		});
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+};
+
+// Get github repos
+export const getGithubRepos = (username) => async (dispatch) => {
+	try {
+		const res = await api.get(`/profile/github/${username}`);
+
+		dispatch({
+			type: GET_REPOS,
+			payload: res.data,
+		});
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+};
+
+// Get all profiles
+export const getProfiles = () => async (dispatch) => {
+	dispatch({ type: CLEAR_PROFILE });
+	try {
+		const res = await api.get("/profile");
+
+		dispatch({
+			type: GET_PROFILES,
+			payload: res.data,
+		});
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+};
+
+// Get profile by ID
+export const getProfileById = (userId) => async (dispatch) => {
+	try {
+		const res = await api.get(`/profile/user/${userId}`);
+
+		dispatch({
+			type: GET_PROFILES,
 			payload: res.data,
 		});
 	} catch (err) {
